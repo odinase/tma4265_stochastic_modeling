7bf19d2dfbef333d0f9942a32deaf4ed0ee57429
andersfagerli <anderstf@stud.ntnu.no>
2019-09-23 14:42:48 +0200

    Added plot with realizations of X(t)

--- task2.R
+++ task2.R
@@ -10,44 +10,47 @@ claims = 100;
 
 prob = ppois(claims, mu, lower.tail = FALSE)
 
-### Simulate realizations and estimate probability
-N = 1000;         #Repeat simulation N times to get a representative estimate
-numSim = 1000;    #Number of realizations
-probs = 0;        #Vector of estimated probabilites for each simulation in 2a)
-expectations = 0; #Vector of estimated expected values for each simulation in 2b)
-variances = 0;    #Vector of estimated variances for each simulation in 2b)
+### Simulate realizations and estimate probability/expectation/variance in 2a/2b
+N = 1000;                  #Repeat simulation N times to get a representative estimate
+numSim = 1000;             #Number of realizations
+probs = numeric(N);        #Vector of estimated probabilites for each simulation in 2a)
+expectations = numeric(N); #Vector of estimated expected values for each simulation in 2b)
+variances = numeric(N);    #Vector of estimated variances for each simulation in 2b)
 beta = 10;
 
 for (i in 1:N){
   sim = rpois(numSim, mu);
   probs[i] = sum(sim > claims)/numSim;
-  expectations[i] = sim/beta;
-  variances[i] = 2*sim/(beta^2);
+  expectations[i] = sim/beta;     #Formula for expectation after law of total expectation
+  variances[i] = 2*sim/(beta^2);  #Formula for variance after law of total variance
 }
 
-upperProb = max(probs);
-lowerProb = min(probs);
-expectedProb = mean(probs);
+cat(sprintf("Probability std: %f\n", sd(probs)));
+cat(sprintf("Probability mean: %f\n", mean(probs)));
 
-upperExpectation = max(expectations);
-lowerExpectation = min(expectations);
-expectedExpectation = mean(expectations);
+cat(sprintf("Expectation std: %f\n", sd(expectations)));
+cat(sprintf("Expectation mean: %f\n", mean(expectations)));
 
-upperVariance = max(variances);
-lowerVariance = min(variances);
-expectedVariance = mean(variances);
+cat(sprintf("Variance std: %f\n", sd(variances)));
+cat(sprintf("Variance mean: %f\n", mean(variances)));
 
-cat(sprintf("Upper probability: %f\n", upperProb))
-cat(sprintf("Lower probability: %f\n", lowerProb))
-cat(sprintf("Mean probability: %f\n",  expectedProb))
+### Figure with realizations of X(t)
+realizations = 10;
+nP = matrix(, nrow = realizations, ncol = 60);
 
-cat(sprintf("Upper expectation: %f\n", upperExpectation))
-cat(sprintf("Lower expectation: %f\n", lowerExpectation))
-cat(sprintf("Mean expectation: %f\n",  expectedExpectation))
+# Generate data
+T = 0:59;
+for (i in 1:realizations) {
+  for (t in T) {
+    nP[i,(t+1)] = rpois(1, t*lambda);
+  }
+}
 
-cat(sprintf("Upper variance: %f\n", upperVariance))
-cat(sprintf("Lower variance: %f\n", lowerVariance))
-cat(sprintf("Mean variance: %f\n",  expectedVariance))
+# Plot data
+plot(NULL, NULL, xlim = c(0, 59), ylim = c(0, max(nP)), xlab = "Time", ylab = "Events", main = "10 realizations of X(t)")
+for (i in 1:realizations) {
+  for (t in T) {
+    lines(T[(t+1):((t+1)+1)], rep(nP[i,t+1],2), col = 31+i)
 
-#HOW TO MAKE FIGURE SHOWING 10 realizations??
-
+  }
+}
