b9fe3a3ec57f94b8131d2d29095fd1fa8bad4656
Odin Aleksander Severinsen <odin@severinsen.no>
2019-10-04 17:55:48 +0200

    Fix bug in expected time simulation

--- problem1d.r
+++ problem1d.r
@@ -1,9 +1,8 @@
-# library(ggplot2)
+library(ggplot2)
 
 beta <- 0.05;
 gamma <- 0.20;
-N <- 1000;
-N.realizations <- 1000;
+N <- 1000000;
 T.s <- c(1:N);
 T.i <- c(1:N);
 p <- 0;
@@ -41,3 +40,10 @@ cat(sprintf("Average time in state S: %f\n", mean(T.s)));
 cat(sprintf("Average time in state I: %f\n", mean(T.i)));
 cat(sprintf("Standard deviation of time in state S: %f\n", sd(T.s)));
 cat(sprintf("Standard deviation of time in state I: %f\n", sd(T.i)));
+
+t <- seq(from=0, to=360, by=0.1);
+y <- exp(t);
+dev.new();
+ggplot(data.frame(T.s), aes(x=T.s)) + geom_histogram(aes(y=..density..), binwidth=1);
+lines(t, y, type='l', col='blue', lwd = 3.5);
+dev.off();
