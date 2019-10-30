4aa826c43984c8c0762ec3a2467885f91e743758
andersfagerli <anderstf@stud.ntnu.no>
2019-10-30 13:39:58 +0100

    Added unfinished prob 1c

---
+++ project2/problem1c.R
@@ -0,0 +1,35 @@
+#################
+### Problem 1 ###
+#################
+
+### Problem 1c)
+
+# Time period
+days = 365; # Ignoring leap years
+years = 5;
+
+# Rates
+lambda = 1/100;
+mu = 1/7;
+rates = c(lambda, mu);
+
+# State (1: Susceptible, 2: Infected)
+xVals = c(1); # 0: Start as susceptible
+tTimes = c(0);
+
+for (i in 1:(days*years)) {
+    currState = xVals[i];
+    sjTime = rexp(1, rate = rates[currState]);
+    nextState = currState %% 2 + 1; # Switch state
+
+    xVals = c(xVals, nextState);
+    tTimes = c(tTimes, tail(tTimes,1)+sjTime);
+}
+
+# Plotting
+
+plot(NULL, NULL, xlim = c(0, days*years), ylim = c(0.8, 2.2), xlab = "Time (days)", ylab = "State", cex.lab = 1.5, cex.axis = 1.5)
+  for(i in 1:(length(xVals)-1)){
+    lines(tTimes[i:(i+1)], rep(xVals[i], 2), lwd = 4)
+  }
+  lines(tail(tTimes, 1) + c(0,1), c(1,1), lwd = 4)
