c1a43e051dd3976a180a24ef4476e011fdec52e2
andersfagerli <anderstf@stud.ntnu.no>
2019-11-06 14:36:52 +0100

    Updated for-loops to run for correct amount of time

--- project2/problem1c.R
+++ project2/problem1c.R
@@ -15,23 +15,29 @@ mu = 1/7;
 rates = c(lambda, mu);
 
 # State (1: Susceptible, 2: Infected)
-xVals = c(1); # Start as susceptible
+xVals = numeric(days*years)
+xVals[1] = 1; # Start as susceptible
+i = 1;
 tTimes = c(0);
+totTime = 0;
 
-for (i in 1:(days*years)) {
+while (totTime <= (days*years)) {
     currState = xVals[i];
     sjTime = rexp(1, rate = rates[currState]);
     nextState = currState %% 2 + 1; # Switch state
 
-    xVals = c(xVals, nextState);
+    xVals[i+1] = nextState;
     tTimes = c(tTimes, tail(tTimes,1)+sjTime);
+    
+    totTime = totTime + sjTime;
+    i = i + 1;
 }
 
 # Plotting
 
 plot(NULL, NULL, xlim = c(0, days*years), ylim = c(0.8, 2.2), xlab = "Time (days)", ylab = "State", cex.lab = 1.5, cex.axis = 1.5)
   for(i in 1:(length(xVals)-1)){
-    lines(tTimes[i:(i+1)], rep(xVals[i], 2), lwd = 4)
+    lines(tTimes[i:(i+1)], rep(xVals[i], 2), lwd = 4, type = "l")
   }
   lines(tail(tTimes, 1) + c(0,1), c(1,1), lwd = 4)
 
@@ -43,23 +49,26 @@ plot(NULL, NULL, xlim = c(0, days*years), ylim = c(0.8, 2.2), xlab = "Time (days
 years = 1000;
 
 # State
-xVals = c(1);
+xVals = numeric(days*years);
+xVals[1] = 1;
+i = 1;
 totTime = 0;      # Total time the simulation runs
 infectedTime = 0; # Time the individual is infected out of the total time
 
-for (i in 1:(days*years)) {
+while (totTime <= (days*years)) {
   currState = xVals[i];
   sjTime = rexp(1, rate = rates[currState]);
   nextState = currState %% 2 + 1; # Switch state
 
-  xVals = c(xVals, nextState);
+  xVals[i+1] = nextState;
 
   if (currState == 2) { # If infected; update infected time
     infectedTime = infectedTime + sjTime;
   }
-
+  
   totTime = totTime + sjTime;
+  i = i + 1;
 }
 
 fracInfected = infectedTime / totTime;
-print(fracInfected);
+print(fracInfected)
